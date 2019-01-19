93c1e9c0c47f89a6b8c6018dd131f965d9850843
Paellis12 <patrick.ellis84@yahoo.com>
2019-01-19 12:21:57 -0500

    made changes to html, css , and js

---
+++ assets/javascript/main.js
@@ -0,0 +1,21 @@
+//Need to create object with questions, answers, and possible answers
+//Then create start button to bring user to questions
+//Then use if and else statements to conclude what will happen when an answer is picked
+//Then create a game over message with correct and incorrect answers, and a button to start over
+
+
+
+
+
+
+$(document).ready(function) {
+    var game = {
+        question: "In The Fellowship of the Ring, what is the name of the ferry the hobbits use to escape the Black Riders?",
+        possibleAnswer: ["Buckleberry Ferry", ]
+    }
+    //grab id from html and give start button functionality//
+    $("#startGame").on("click", function) {
+
+    }
+
+}
